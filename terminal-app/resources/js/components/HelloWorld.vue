4ba233da2286c2818c4db9bd292a34b50684acb1
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-08 14:50:59 +0330

    Vue terminal added but not good

--- terminal-app/resources/js/components/HelloWorld.vue
+++ terminal-app/resources/js/components/HelloWorld.vue
@@ -3,12 +3,17 @@
         <h1>
             Hi world!
         </h1>
+        <vue-terminal title="Online Terminal" />
     </div>
 </template>
 
 <script>
+    import VueTerminal from 'vue-terminal';
     export default {
-        name: "HelloWorld"
+        name: "HelloWorld",
+        components: {
+            VueTerminal
+        }
     }
 </script>
 
