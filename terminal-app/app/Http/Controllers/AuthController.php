4b2887f1139b2844f032f7945135775d6906aa75
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-26 18:42:07 +0330

    Auth login added

--- terminal-app/app/Http/Controllers/AuthController.php
+++ terminal-app/app/Http/Controllers/AuthController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use TheSeer\Tokenizer\Token;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request): JsonResponse
+    {
+        $name = $request->get('name');
+
+        if (User::query()->where('name', '=', $name)->exists()) {
+            $token = (new Token(1, $name, $name));
+            session($name, $token);
+        } else {
+            $token = 'None';
+        }
+
+        return response()
+            ->json([
+                'token' => $token
+            ]);
+    }
 }
