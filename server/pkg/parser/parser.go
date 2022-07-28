2446b817275764702ca11d9edac257428d4afa93
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-28 18:15:12 +0430

    add: parser

--- server/pkg/parser/parser.go
+++ server/pkg/parser/parser.go
@@ -1,12 +1,19 @@
 package parser
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	separator = " "
 )
 
 func ParseCommand(command string) ([]string, error) {
+	if len(command) == 0 {
+		return nil, fmt.Errorf("empty command")
+	}
+
 	arg := strings.Split(command, separator)
 
 	return arg, nil
