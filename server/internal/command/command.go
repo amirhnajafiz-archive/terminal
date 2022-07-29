b1fe290d1567d4ec388b6051ea99b61276328157
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-29 10:26:59 +0430

    add: command register

--- server/internal/command/command.go
+++ server/internal/command/command.go
@@ -1,10 +1,25 @@
 package command
 
-import "github.com/amirhnajafiz/terminal/server/internal/types"
+import (
+	"fmt"
+	"time"
 
-func NewCommand(u string, a func([]string) error) *types.Command {
+	"github.com/amirhnajafiz/terminal/server/internal/types"
+)
+
+func NewCommand(u string, a func([]string) (string, error)) *types.Command {
 	return &types.Command{
 		Use:    u,
 		Action: a,
+	}
+}
+
+func timer(_ []string) (string, error) {
+	return fmt.Sprintln(time.Now()), nil
+}
+
+func Register() []*types.Command {
+	return []*types.Command{
+		NewCommand("time", timer),
 	}
 }
--- server/internal/types/command.go
+++ server/internal/types/command.go
@@ -1,6 +1,6 @@
 package types
 
 type Command struct {
-	Use    string                    `json:"use"`
-	Action func(args []string) error `json:"action"`
+	Use    string                              `json:"use"`
+	Action func(args []string) (string, error) `json:"action"`
 }
