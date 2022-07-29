908beeee0a80d5ef5255a151391ee3667fa5e6f3
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-29 10:36:52 +0430

    init: http

--- server/internal/cmd/main.go
+++ server/internal/cmd/main.go
@@ -1,5 +1,24 @@
 package cmd
 
-func Execute() {
+import (
+	"log"
+	"os/user"
+	"runtime"
 
+	"github.com/amirhnajafiz/terminal/server/internal/command"
+	"github.com/amirhnajafiz/terminal/server/internal/command/handler"
+)
+
+func Execute() {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	h := handler.Handler{
+		User: u.Username,
+		OS:   runtime.GOOS,
+	}
+
+	command.Register(h)
 }
