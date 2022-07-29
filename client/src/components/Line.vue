58b6cf8ecdfca2d6784bbf4633d45b1d6f30521a
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-29 11:23:10 +0430

    add: line

---
+++ client/src/components/Line.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  data: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Line"
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/Terminal.vue
+++ client/src/components/Terminal.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="container">
-    Terminal
+    <Line
+        v-for="line in this.lines"
+        :data="'$/ ' + line"
+    >
+    </Line>
   </div>
 </template>
 
 <script>
+import Line from "@/components/Line.vue";
+
 export default {
-  name: "Terminal"
+  name: "Terminal",
+  components: {Line},
+  data() {
+    return {
+      lines: [
+          "hello",
+      ]
+    }
+  },
+  methods: {
+    addCommand(str) {
+      this.lines.push(str)
+    }
+  }
 }
 </script>
 
 <style scoped>
 .container {
   width: 100%;
-  padding: 5px;
+  padding: 5px 10px;
   background: #ffffff;
   color: #222222;
+  border-radius: 4px;
+
+  max-height: 500px;
+  overflow: scroll;
 }
 </style>
