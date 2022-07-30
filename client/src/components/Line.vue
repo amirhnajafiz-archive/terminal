31fa2eccc4a82dda9644c0db6c61e5134fcc2d6d
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-30 08:53:53 +0430

    add: line types

--- client/src/components/Line.vue
+++ client/src/components/Line.vue
@@ -3,22 +3,47 @@ defineProps({
   data: {
     type: String,
     required: true
+  },
+  type: {
+    type: String,
   }
 })
 </script>
 
 <template>
-  <div>
+  <div :class="this.getType">
     {{ data }}
   </div>
 </template>
 
 <script>
 export default {
-  name: "Line"
+  name: "Line",
+  methods: {
+    getType() {
+      switch (this.type) {
+        case "danger":
+          return "danger"
+        case "success":
+          return "success"
+        default:
+          return "normal"
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
+.normal {
+  color: #222222;
+}
 
+.danger {
+  color: #aa1111;
+}
+
+.success {
+  color: #008800;
+}
 </style>
